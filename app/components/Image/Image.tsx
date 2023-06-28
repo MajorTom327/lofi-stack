44ef824f5cf03bd9653ec021c39868b6043616ac
majortom327 <me@valentin-thomas.com>
2023-06-28 12:31:54 +0200

    Update remix config

---
+++ app/components/Image/Image.tsx
@@ -0,0 +1,55 @@
+import { propOr } from "ramda";
+import React from "react";
+
+type Props = {
+  src: string;
+  alt?: string;
+};
+
+type ImageWidth = 640 | 750 | 828 | 1080 | 1200;
+
+const imageWidths: ImageWidth[] = [640, 750, 828, 1080, 1200];
+
+type Options = Partial<{
+  width: ImageWidth;
+  quality: number;
+}>;
+
+export const getImageUrl = (src: string, options?: Options) => {
+  if (process.env.NODE_ENV === "development") {
+    return src;
+  }
+
+  const url = new URL("/_vercel/image", process.env.APP_URL);
+
+  url.searchParams.append("url", src);
+
+  const width: number = propOr(1200, "width", options);
+  const quality: number = propOr(75, "quality", options);
+
+  url.searchParams.append("w", width.toString());
+  url.searchParams.append("q", quality.toString());
+
+  return url.toString();
+};
+
+export const Image: React.FC<Props> = ({ src, alt }) => {
+  return (
+    <>
+      <picture>
+        {imageWidths.map((width) => (
+          <source
+            key={width}
+            srcSet={getImageUrl(src, { width })}
+            media={`(min-width: ${width}px)`}
+          />
+        ))}
+        <img src={getImageUrl(src)} alt={alt} />
+      </picture>
+    </>
+  );
+};
+
+Image.defaultProps = {};
+
+export default Image;
