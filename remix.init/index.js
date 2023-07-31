770c5fddf4bff14b2b9ac984cdc307a1cbf6c989
majortom327 <me@valentin-thomas.com>
2023-07-31 11:01:07 +0200

    Use dirname instead

--- remix.init/index.js
+++ remix.init/index.js
@@ -2,43 +2,45 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const path = require("path");
 
-(() => {
-  const cwd = process.cwd();
-  fs.copyFileSync(path.resolve(cwd, ".env.dist"), path.resolve(cwd, ".env"));
+console.log("🚀  Initializing your project...", __dirname);
+const cwd = path.resolve(__dirname, "..");
+fs.copyFileSync(path.resolve(cwd, ".env.dist"), path.resolve(cwd, ".env"));
 
-  inquirer
-    .prompt([
-      {
-        type: "confirm",
-        name: "dependabot",
-        message: "Do you want to add dependabot?",
-        default: true,
-      },
-      {
-        type: "input",
-        name: "reviewer",
-        message: "What is your github username?",
-        default: "octocat",
-      },
-      {
-        type: "confirm",
-        name: "husky",
-        message: "Do you want to add husky?",
-        default: true,
-      },
-    ])
-    .then((answers) => {
-      if (answers.husky) {
-        pkg.scripts.prepare = "husky install";
-        fs.writeFileSync(
-          path.resolve(cwd, "package.json"),
-          JSON.stringify(pkg, null, 2)
-        );
-      }
+const pkg = require(path.resolve(cwd, "package.json"));
 
-      if (answers.dependabot) {
-        const username = answers.reviewer;
-        const dependabot = `
+inquirer
+  .prompt([
+    {
+      type: "confirm",
+      name: "dependabot",
+      message: "Do you want to add dependabot?",
+      default: true,
+    },
+    {
+      type: "input",
+      name: "reviewer",
+      message: "What is your github username?",
+      default: "octocat",
+    },
+    {
+      type: "confirm",
+      name: "husky",
+      message: "Do you want to add husky?",
+      default: true,
+    },
+  ])
+  .then((answers) => {
+    if (answers.husky) {
+      pkg.scripts.prepare = "husky install";
+      fs.writeFileSync(
+        path.resolve(cwd, "package.json"),
+        JSON.stringify(pkg, null, 2)
+      );
+    }
+
+    if (answers.dependabot) {
+      const username = answers.reviewer;
+      const dependabot = `
   version: 2
   updates:
     - package-ecosystem: github-actions
@@ -55,34 +57,30 @@ const path = require("path");
         - "${username}"
   `;
 
-        fs.writeFileSync(
-          path.resolve(cwd, ".github/dependabot.yml"),
-          dependabot
-        );
-      }
+      fs.writeFileSync(path.resolve(cwd, ".github/dependabot.yml"), dependabot);
+    }
 
-      const actionContent = fs
-        .readFileSync(path.resolve(cwd, ".github/workflows/build.yml"))
-        .toString();
+    const actionContent = fs
+      .readFileSync(path.resolve(cwd, ".github/workflows/build.yml"))
+      .toString();
 
-      // Re-enable cache for yarn as it is disabled for the stack
-      const newAction = actionContent.replace(
-        /# cache: "yarn"/g,
-        `cache: "yarn"`
-      );
+    // Re-enable cache for yarn as it is disabled for the stack
+    const newAction = actionContent.replace(
+      /# cache: "yarn"/g,
+      `cache: "yarn"`
+    );
 
-      fs.writeFileSync(
-        path.resolve(cwd, ".github/workflows/build.yml"),
-        newAction
-      );
+    fs.writeFileSync(
+      path.resolve(cwd, ".github/workflows/build.yml"),
+      newAction
+    );
 
-      console.log("🎉  Done!");
-    })
-    .catch((error) => {
-      if (error.isTtyError) {
-        // Prompt couldn't be rendered in the current environment
-      } else {
-        // Something else went wrong
-      }
-    });
-})();
+    console.log("🎉  Done!");
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
