a3af0f6b2a377d221c5f467981f603a802f65a7a
majortom327 <me@valentin-thomas.com>
2023-07-31 11:21:13 +0200

    Use main as export

--- remix.init/index.js
+++ remix.init/index.js
@@ -2,86 +2,96 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const path = require("path");
 
-console.log("🚀  Initializing your project...");
-const cwd = path.resolve(__dirname, "..");
+const main = () => {
+  console.log("🚀  Initializing your project...");
+  const cwd = path.resolve(__dirname, "..");
 
-inquirer
-  .prompt([
-    {
-      type: "confirm",
-      name: "dependabot",
-      message: "Do you want to add dependabot?",
-      default: true,
-    },
-    {
-      type: "input",
-      name: "reviewer",
-      message: "What is your github username?",
-      default: "octocat",
-    },
-    {
-      type: "confirm",
-      name: "husky",
-      message: "Do you want to add husky?",
-      default: true,
-    },
-  ])
-  .then((answers) => {
-    fs.copyFileSync(path.resolve(cwd, ".env.dist"), path.resolve(cwd, ".env"));
-
-    const pkg = require(path.resolve(cwd, "package.json"));
-
-    if (answers.husky) {
-      pkg.scripts.prepare = "husky install";
-      fs.writeFileSync(
-        path.resolve(cwd, "package.json"),
-        JSON.stringify(pkg, null, 2)
+  inquirer
+    .prompt([
+      {
+        type: "confirm",
+        name: "dependabot",
+        message: "Do you want to add dependabot?",
+        default: true,
+      },
+      {
+        type: "input",
+        name: "reviewer",
+        message: "What is your github username?",
+        default: "octocat",
+      },
+      {
+        type: "confirm",
+        name: "husky",
+        message: "Do you want to add husky?",
+        default: true,
+      },
+    ])
+    .then((answers) => {
+      fs.copyFileSync(
+        path.resolve(cwd, ".env.dist"),
+        path.resolve(cwd, ".env")
       );
-    }
 
-    if (answers.dependabot) {
-      const username = answers.reviewer;
-      const dependabot = `
-  version: 2
-  updates:
-    - package-ecosystem: github-actions
-      directory: /
-      schedule:
-        interval: daily
-      reviewers:
-        - "${username}"
-    - package-ecosystem: "npm" # See documentation for possible values
-      directory: "/" # Location of package manifests
-      schedule:
-        interval: "daily"
-      reviewers:
-        - "${username}"
-  `;
+      const pkg = require(path.resolve(cwd, "package.json"));
 
-      fs.writeFileSync(path.resolve(cwd, ".github/dependabot.yml"), dependabot);
-    }
+      if (answers.husky) {
+        pkg.scripts.prepare = "husky install";
+        fs.writeFileSync(
+          path.resolve(cwd, "package.json"),
+          JSON.stringify(pkg, null, 2)
+        );
+      }
 
-    const actionContent = fs
-      .readFileSync(path.resolve(cwd, ".github/workflows/build.yml"))
-      .toString();
+      if (answers.dependabot) {
+        const username = answers.reviewer;
+        const dependabot = `
+    version: 2
+    updates:
+      - package-ecosystem: github-actions
+        directory: /
+        schedule:
+          interval: daily
+        reviewers:
+          - "${username}"
+      - package-ecosystem: "npm" # See documentation for possible values
+        directory: "/" # Location of package manifests
+        schedule:
+          interval: "daily"
+        reviewers:
+          - "${username}"
+    `;
 
-    // Re-enable cache for yarn as it is disabled for the stack
-    const newAction = actionContent.replace(
-      /# cache: "yarn"/g,
-      `cache: "yarn"`
-    );
+        fs.writeFileSync(
+          path.resolve(cwd, ".github/dependabot.yml"),
+          dependabot
+        );
+      }
 
-    fs.writeFileSync(
-      path.resolve(cwd, ".github/workflows/build.yml"),
-      newAction
-    );
+      const actionContent = fs
+        .readFileSync(path.resolve(cwd, ".github/workflows/build.yml"))
+        .toString();
 
-    console.log("🎉  Done!");
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
+      // Re-enable cache for yarn as it is disabled for the stack
+      const newAction = actionContent.replace(
+        /# cache: "yarn"/g,
+        `cache: "yarn"`
+      );
+
+      fs.writeFileSync(
+        path.resolve(cwd, ".github/workflows/build.yml"),
+        newAction
+      );
+
+      console.log("🎉  Done!");
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+      } else {
+        // Something else went wrong
+      }
+    });
+};
+
+module.exports = main;
