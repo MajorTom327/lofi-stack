c29cadf6529959f07c7fa73d1e0178e46dad22ed
majortom327 <me@valentin-thomas.com>
2023-08-17 11:45:49 +0200

    Fix breaking changes

--- test/setup-test-env.ts
+++ test/setup-test-env.ts
@@ -1,5 +1,5 @@
 import { installGlobals } from "@remix-run/node";
-import "@testing-library/jest-dom/extend-expect";
+import "@testing-library/jest-dom/vitest";
 import dotenv from "dotenv";
 import { vi } from "vitest";
 
